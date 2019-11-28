9e0ffe0c88afa2b524230c826ed9445b17a4578a
Mikhail <sadsnake@mixbytes.io>
2019-11-28 15:45:54 +0300

    change `add` to `checked_add` in module

--- runtime/src/oracle.rs
+++ runtime/src/oracle.rs
@@ -6,8 +6,10 @@ use support::{
 use support::codec::{Decode, Encode};
 use timestamp;
 
+use rstd::result;
+
 use rstd::vec::Vec;
-use runtime_primitives::traits::{Member, One, SimpleArithmetic};
+use runtime_primitives::traits::{CheckedAdd, Member, One, SimpleArithmetic};
 use system::ensure_signed;
 
 type AssetName = Vec<u8>;
@@ -15,7 +17,7 @@ type AssetName = Vec<u8>;
 pub trait Trait: timestamp::Trait {
     type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
     type ExternalValueType: Member + Parameter + SimpleArithmetic + Default + Copy;
-    type OracleId: Parameter + SimpleArithmetic + Default + Copy;
+    type OracleId: Parameter + Member + SimpleArithmetic + Default + Copy;
 }
 
 #[derive(Encode, Decode)]
@@ -42,13 +44,21 @@ decl_storage! {
     trait Store for Module<T: Trait> as Oracle {
         NextOracleId get(last_oracle_id) build(|conf: &GenesisConfig<T>| {
             let mut res = T::OracleId::default();
-            for _ in 0..conf.default_oracles.len() { res += One::one(); };
+            for _ in 0..conf.default_oracles.len() {
+                res = res.checked_add(&One::one()).expect("Too much oracles in config, T::OracleId overflow.");
+            }
             res
         }): T::OracleId;
 
         pub OraclesMap get(oracles) build(|conf: &GenesisConfig<T>| {
-            let oracle_id = T::OracleId::default();
-            let post_inc = |id: &mut T::OracleId| {let tmp = id.clone(); id += One::one; tmp};
+            let mut oracle_id = T::OracleId::default();
+
+            let post_inc = |id: &mut T::OracleId| {
+                let tmp = id.clone();
+                id.checked_add(&One::one()).expect("Too much oracles in config, T::OracleId overflow.");
+                tmp
+            };
+
             conf.default_oracles.iter().map(|&(ref source_account, ref external_name)| {
                 (post_inc(&mut oracle_id),
                  OracleData { source_account: source_account.clone(),
@@ -72,7 +82,7 @@ decl_module! {
             let who = ensure_signed(origin)?;
 
             if <OraclesMap<T>>::get(oracle_id).source_account != who {
-                Err("Can't commit price: no permission")
+                Err("Can't commit external value: no permission")
             } else {
                 <OraclesMap<T>>::mutate(oracle_id, |data| {
                     data.external_value = Some((new_external_value, <timestamp::Module<T>>::get()));
@@ -85,7 +95,7 @@ decl_module! {
         pub fn create_oracle(origin, external_name: Vec<u8>, start_external_value: Option<T::ExternalValueType>) {
             let who: T::AccountId = ensure_signed(origin)?;
 
-            <OraclesMap<T>>::insert(Self::get_next_oracle_id(),
+            <OraclesMap<T>>::insert(Self::get_next_oracle_id()?,
                 OracleData {
                     source_account: who,
                     external_name: external_name,
@@ -108,10 +118,15 @@ decl_event!(
 );
 
 impl<T: Trait> Module<T> {
-    fn get_next_oracle_id() -> T::OracleId {
-        let id: T::OracleId = Self::last_oracle_id();
-        <NextOracleId<T>>::mutate(|id| *id += One::one());
-        id
+    fn get_next_oracle_id() -> result::Result<T::OracleId, &'static str> {
+        let mut result = Ok(Self::last_oracle_id());
+
+        NextOracleId::<T>::mutate(|id| match id.checked_add(&One::one()) {
+            Some(res) => *id = res,
+            None => result = Err("T::OracleId overflow. Can't get next id."),
+        });
+
+        result
     }
 
     pub fn get_max_oracle_id() -> Option<T::OracleId> {
